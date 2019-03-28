8e618cedb6db0640f3ef6904cbd484f22d82a242
juddx097 <juddx097@morris.umn.edu>
2019-03-28 14:30:07 -0500

    Made it so that the html for edit-ride can show up

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,12 +5,14 @@ import {RideListComponent} from "./rides/ride-list.component";
 import {AddRideComponent} from "./rides/add-ride.component";
 import {AppComponent} from "./app.component";
 import {HomeComponent} from "./home/home.component";
+import {EditRideComponent} from "./edit-ride/edit-ride.component";
 
 // Route Configuration
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'rides', component: RideListComponent},
-  {path: 'addride', component: AddRideComponent}
+  {path: 'addride', component: AddRideComponent},
+  {path: 'editride', component: EditRideComponent}
 
 ];
 
--- client/src/app/edit-ride/edit-ride.component.ts
+++ client/src/app/edit-ride/edit-ride.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {Ride} from '../rides/ride';
+import {FormControl, Validators, FormGroup, FormBuilder} from "@angular/forms";
+
+//editing a ride should have the same validation as adding a ride
+import {AddRideComponent} from "../rides/add-ride.component";
+
+import {Observable} from "rxjs/Observable";
+import {RideListComponent} from "../rides/ride-list.component";
+import {RideListService} from "../rides/ride-list.service";
+
 
 @Component({
   selector: 'app-edit-ride',
   templateUrl: './edit-ride.component.html',
-  styleUrls: ['./edit-ride.component.css']
+  styleUrls: ['./edit-ride.component.css'],
+  providers: [ RideListComponent ],
 })
+
+
 export class EditRideComponent implements OnInit {
 
   constructor() { }
