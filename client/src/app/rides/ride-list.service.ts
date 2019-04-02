3e93b640720389597d427d45601c4270425cc921
michael-small <small203@morris.umn.edu>
2019-04-02 18:07:21 -0500

    Establish server methods to edit rides and retrieve rides
    Edit uses a POST, but it may need to be a PUT. Retrieve is a GET.
    For the moment, retrieve is hard coded because I have no idea if I am
    passing in the right things and am new to GET requests.
    
    #4

--- client/src/app/rides/ride-list.service.ts
+++ client/src/app/rides/ride-list.service.ts
@@ -84,7 +84,32 @@ export class RideListService {
 
     //this.rlc.refreshRides();
     const id = this.http.post<string>(this.rideUrl + '/new', newRide, httpOptions);
+    console.log("ADD (POST) ID: " + id);
     return id;
+  }
 
+  editExistingRide(existingRideEdited: Ride, idOfRide: string): Observable<string> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        // We're sending JSON
+        'Content-Type': 'application/json'
+      }),
+      // But we're getting a simple (text) string in response
+      // The server sends the hex version of the new ride back
+      // so we know how to find/access that user again later.
+      responseType: 'text' as 'json'
+    };
+
+    const id = this.http.post<string>(this.rideUrl + '/' + idOfRide, existingRideEdited, httpOptions);
+    console.log("EDIT (POST) ID: ");
+    return id;
+  }
+
+  retrieveExistingRide(idOfRide: string): Observable<string> {
+
+    // const id = this.http.get<string>(this.rideUrl + '/' + idOfRide, httpOptions);
+    const id = this.http.get<string>(this.rideUrl + '/5c832bec3f173391643a15f1');
+    console.log("RETRIEVE (GET) ID: " + id);
+    return id;
   }
 }
